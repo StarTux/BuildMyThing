9cc54126c17ca437267914be1b84f6fcc86f009c
StarTux <ths.loy@gmail.com>
2025-07-27 00:43:54 +0200

    Protect buckets, placed entities, and spawn eggs

--- src/main/java/com/cavetale/buildmything/GameListener.java
+++ src/main/java/com/cavetale/buildmything/GameListener.java
@@ -3,20 +3,28 @@ package com.cavetale.buildmything;
 import com.cavetale.core.event.block.PlayerBlockAbilityQuery;
 import com.cavetale.core.event.block.PlayerBreakBlockEvent;
 import com.cavetale.core.event.hud.PlayerHudEvent;
+import com.destroystokyo.paper.MaterialTags;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.minecart.CommandMinecart;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityPlaceEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 @RequiredArgsConstructor
 public final class GameListener implements Listener {
@@ -46,15 +54,38 @@ public final class GameListener implements Listener {
         checkBlockEdit(event.getPlayer(), event.getBlock(), event);
     }
 
-    @EventHandler
-    private void onPlayerHud(PlayerHudEvent event) {
-        if (event.getPlayer().getWorld().equals(Bukkit.getWorlds().get(0))) {
-            plugin.onPlayerHud(event);
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    private void onEntityPlace(EntityPlaceEvent event) {
+        if (event.getEntity() instanceof CommandMinecart) {
+            plugin.getLogger().warning(event.getPlayer().getName() + " tried placing Command Minecart");
+            event.setCancelled(true);
             return;
         }
-        final Game game = Game.in(event.getPlayer().getWorld());
-        if (game == null || game.getMode() == null) return;
-        game.onPlayerHud(event);
+        checkBlockEdit(event.getPlayer(), event.getEntity().getLocation().getBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    private void onPlayerBucketFill(PlayerBucketFillEvent event) {
+        checkBlockEdit(event.getPlayer(), event.getBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    private void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
+        checkBlockEdit(event.getPlayer(), event.getBlock(), event);
+    }
+
+    /**
+     * Control spawn eggs.
+     */
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    private void onPlayerInteractSpawnEgg(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK || !event.hasBlock() || !event.hasItem()) {
+            return;
+        }
+        if (!MaterialTags.SPAWN_EGGS.isTagged(event.getItem().getType())) {
+            return;
+        }
+        checkBlockEdit(event.getPlayer(), event.getClickedBlock().getRelative(event.getBlockFace()), event);
     }
 
     private static void checkBlockEdit(Player player, Block block, Cancellable event) {
@@ -69,10 +100,21 @@ public final class GameListener implements Listener {
         }
     }
 
+    @EventHandler
+    private void onPlayerHud(PlayerHudEvent event) {
+        if (event.getPlayer().getWorld().equals(Bukkit.getWorlds().get(0))) {
+            plugin.onPlayerHud(event);
+            return;
+        }
+        final Game game = Game.in(event.getPlayer().getWorld());
+        if (game == null || game.getMode() == null) return;
+        game.onPlayerHud(event);
+    }
+
     /**
      * Avoid falling blocks.
      */
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     private void onEntityChangeBlock(EntityChangeBlockEvent event) {
         final Game game = Game.in(event.getEntity().getWorld());
         if (game == null) return;
@@ -84,7 +126,7 @@ public final class GameListener implements Listener {
     /**
      * Avoid falling blocks.
      */
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     private void onEntitySpawn(EntitySpawnEvent event) {
         final Game game = Game.in(event.getEntity().getWorld());
         if (game == null) return;
@@ -98,14 +140,34 @@ public final class GameListener implements Listener {
     /**
      * Avoid spill.
      */
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     private void onBlockFromTo(BlockFromToEvent event) {
         final Game game = Game.in(event.getBlock().getWorld());
         if (game == null) return;
+        if (!game.isBuildingAllowed()) {
+            event.setCancelled(true);
+        }
         final BuildArea buildArea = game.findBuildAreaAt(event.getBlock());
         if (buildArea == null || !buildArea.getArea().contains(event.getToBlock())) {
             event.setCancelled(true);
             return;
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    private void onCreatureSpawn(CreatureSpawnEvent event) {
+        final Game game = Game.in(event.getEntity().getWorld());
+        if (game == null) return;
+        switch (event.getSpawnReason()) {
+        case SPAWNER_EGG:
+            if (!game.isBuildingAllowed()) {
+                event.setCancelled(true);
+                return;
+            }
+            event.getEntity().setAI(false);
+            event.getEntity().setGravity(false);
+            event.getEntity().setSilent(true);
+        default: break;
         }
     }
 }
